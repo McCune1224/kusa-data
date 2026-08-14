5c480b77fc4f57ec5f9de2d14e1c9b1cd6f0b365
McCune1224 <alexmccune1224@gmail.com>
2026-08-14 14:47:25 -0400

    chore: nuke old codebase for fresh start

--- lib/kusa_data/mailer.ex
+++
@@ -1,3 +0,0 @@
-defmodule KusaData.Mailer do
-  use Swoosh.Mailer, otp_app: :kusa_data
-end
--- lib/kusa_data/repo.ex
+++
@@ -1,5 +0,0 @@
-defmodule KusaData.Repo do
-  use Ecto.Repo,
-    otp_app: :kusa_data,
-    adapter: Ecto.Adapters.Postgres
-end
